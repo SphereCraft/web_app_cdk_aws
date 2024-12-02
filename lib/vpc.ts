355e37c265fdf11e9c825f7e70caf1669731f06d
Jeff <j.archerwebb@gmail.com>
2024-12-02 18:20:31 +0000

    Start of the VPC

---
+++ lib/vpc.ts
@@ -0,0 +1,32 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { Construct } from 'constructs';
+
+// Making the VPC available from all files
+export class VPCStack extends cdk.Stack {
+    public readonly vpc: ec2.Vpc;
+    
+    // calling the constructor
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        // Create 2 AZ's with a public and private subnet in each
+        this.vpc = new ec2.Vpc(this, 'WebAppVpc', {
+            maxAzs: 2, // Limits the AZ's to max 2 
+            subnetConfiguration: [
+                {
+                    subnetType: ec2.SubnetType.PUBLIC, // Public subnets for ALB and NAT Gateway
+                        name: 'PublicSubnet',
+                    cidrMask: 24,
+                },
+                {
+                    subnetType: ec2.SubnetType.PRIVATE_ISOLATED, // Private subnets
+                    name: 'PrivateSubnet',
+                    cidrMask: 24,
+                },
+            ],
+        });
+
+
+    }
+}
